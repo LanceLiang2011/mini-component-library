9d7b791a149d9f84203bb6e8ed123c73984c1cc3
Lance Liang <liangxianyi2011@gmail.com>
2023-06-27 00:16:20 -0400

    Complete ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,55 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZE_STYLES = {
+  small: {
+    "--height": "8px",
+    "--border-radius": "4px",
+    "--padding": "0",
+  },
+  medium: {
+    "--height": "12px",
+    "--border-radius": "4px",
+    "--padding": "0",
+  },
+  large: {
+    "--height": "24px",
+    "--border-radius": "4px",
+    "--padding": "4px",
+  },
 };
 
+const ProgressBar = ({ value, size }) => {
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuemin={0}
+      aria-valuemax={100}
+      aria-valuenow={90}
+      style={SIZE_STYLES[size]}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <Bar style={{ "--value": `${value}%` }} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: 0px 2px 4px 0px rgba(128, 128, 128, 0.35) inset;
+  height: var(--height);
+  border-radius: var(--border-radius);
+  padding: var(--padding);
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--value);
+  background-color: ${COLORS.primary};
+  height: 100%;
+`;
 export default ProgressBar;
